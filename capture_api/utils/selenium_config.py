66d560df41f59d741d5ea1c7d83a7807512524f0
DESKTOP-UQ18ON6\ADMIN <xodnr17@g1ncom.co.kr>
2023-12-14 13:34:35 +0900

    안정성 확보 및 특정 영역 캡쳐부분 지정

---
+++ capture_api/utils/selenium_config.py
@@ -0,0 +1,64 @@
+import time
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def elementExclude(driver,className=None,tagname=None, xpath=None):
+    # 특정 요소를 제외하는 함수
+    if className:
+        elements_to_exclude = driver.find_elements(By.CLASS_NAME,className)
+    elif tagname:
+        elements_to_exclude = driver.find_elements(By.TAG_NAME, tagname)
+    elif xpath:
+        elements_to_exclude = driver.find_elements(By.XPATH, xpath)
+    print(elements_to_exclude)
+    # 찾은 요소를 숨기기 (CSS 스타일을 사용하여 숨기기)
+    for element in elements_to_exclude:
+        driver.execute_script("arguments[0].style.visibility = 'hidden';", element)
+
+
+
+def settingDriverSize(driver, width, height):
+    # 드라이버 사이즈를 설정하는 함수
+    driver.execute_script(f"window.innerWidth = {width};")
+    driver.execute_script(f"window.innerHeight = {height};")
+    driver.set_window_size(width, height)
+
+def autoInstaLogin(id,pw):
+    # 자동으로 인스타그램 로그인하는 함수
+    pass
+def autoFaceBookLogin(driver,id,pw):
+    # 자동으로 페이스북 로그인하는 함수
+    driver.get("https://www.facebook.com/")
+    wait = WebDriverWait(driver, 20)
+    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "_42ft")))
+    time.sleep(5)
+    username = driver.find_element(By.ID, "email")
+    print("가져오기1")
+    password = driver.find_element(By.ID, "pass")
+    print("가져오기2")
+    # 파라미터로 전달받은 아이디와 패스워드 값
+    username.send_keys(id)
+    print("페이스북 아이디 입력")
+    password.send_keys(pw)
+    print("페이스북 비밀번호 입력")
+    password.send_keys(Keys.ENTER)
+    time.sleep(5)
+    print(driver.current_url)
+    print("로그인 성공")
+
+
+def autoYouTubeLogin(id,pw):
+    # 자동으로 유튜브 로그인하는 함수
+    pass
+
+def screenShot(filename, driver):
+    # ec2
+    # driver.save_screenshot(f"/home/ec2-user/{filename}.png")
+    driver.save_screenshot(f"C://data/{filename}.png")
+    # print("스크린샷 저장 성공")
+    # local
+    # driver.save_screenshot(f"/Users/marmin/downloads/capture/{filename}.png")
--- capture_api/views.py
+++ capture_api/views.py
@@ -1,214 +1,48 @@
-import time
-
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 
-# ------------------------유틸 함수------------------------------
-def elementExclude(driver,className=None,tagname=None, xpath=None):
-    # 특정 요소를 제외하는 함수
-    if className:
-        elements_to_exclude = driver.find_elements(By.CLASS_NAME,className)
-    elif tagname:
-        elements_to_exclude = driver.find_elements(By.TAG_NAME, tagname)
-    elif xpath:
-        elements_to_exclude = driver.find_elements(By.XPATH, xpath)
-    print(elements_to_exclude)
-    # 찾은 요소를 숨기기 (CSS 스타일을 사용하여 숨기기)
-    for element in elements_to_exclude:
-        driver.execute_script("arguments[0].style.visibility = 'hidden';", element)
 
-def exitHandler(driver):
-    # 장고 프로그램 종료시 드라이버 닫기
-    print("django 프로젝트를 종료합니다")
-    driver.quit()
-
-def settingDriverSize(driver, width, height):
-    # 드라이버 사이즈를 설정하는 함수
-    driver.execute_script(f"window.innerWidth = {width};")
-    driver.execute_script(f"window.innerHeight = {height};")
-    driver.set_window_size(width, height)
-
-def autoInstaLogin(id,pw):
-    # 자동으로 인스타그램 로그인하는 함수
-    pass
-def autoFaceBookLogin(driver,id,pw):
-    # 자동으로 페이스북 로그인하는 함수
-    driver.get("https://www.facebook.com/")
-    wait = WebDriverWait(driver, 20)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "_42ft")))
-    time.sleep(5)
-    username = driver.find_element(By.ID, "email")
-    print("가져오기1")
-    password = driver.find_element(By.ID, "pass")
-    print("가져오기2")
-    # 파라미터로 전달받은 아이디와 패스워드 값
-    username.send_keys(id)
-    print("페이스북 아이디 입력")
-    password.send_keys(pw)
-    print("페이스북 비밀번호 입력")
-    password.send_keys(Keys.ENTER)
-    time.sleep(5)
-    print(driver.current_url)
-    print("로그인 성공")
+from capture_api.utils.capture import capture
 
 
 
-def autoYouTubeLogin(id,pw):
-    # 자동으로 유튜브 로그인하는 함수
-    pass
 
-def screenShot(filename):
-    # ec2
-    # driver.save_screenshot(f"/home/ec2-user/{filename}.png")
-    # print("스크린샷 저장 성공")
-    # local
-    # driver.save_screenshot(f"/Users/marmin/downloads/capture/{filename}.png")
-    # idc center
-    driver.save_screenshot(f"/home/appsvr/capture/test/{filename}.png")
-    # driver.save_screenshot(f"/home/appsvr/capture/test/{filename}.png")
 
-# -----------------Setting-------------------
-# 크롬 옵션 설정하는 부분
-# 크롬 드라이버 자동 실행되도록 (로그인 이슈/ 속도 이슈 해결)
 chrome_options = webdriver.ChromeOptions()
 print("옵션 불러오기 성공")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--lang=ko_KR.utf8')
 chrome_options.add_argument('--hide-scrollbars')
 print("옵션 설정 성공")
-driver = webdriver.Chrome(options=chrome_options)
-# ------- Login -------
-autoFaceBookLogin(driver, "01095528693", "thpo4327")
-# atexit.register(exitHandler(driver))
 
 
 
-# --------- 캡처 함수  --------
 
-def capture(data):
-    '''
-    Post 요청을 통해 이미지 크기, 너비, url, 파일 이름을 받아온 뒤
-    셀레니움 라이브러리를 통해 캡쳐를 진행
-    '''
-    # 요청 데이터 파싱
-    # path = "/Users/marmin/Downloads/chromedriver_mac_arm64/chromedriver"
-
-    url = data.get("urlPath",None)
-    # 전송된 데이터 중 filename을 가져옴
-    filename = data.get("filename", None)
-    print("파싱 성공")
-    # 파일 이름 중복 방지
-    driver.get(url)
-    print("크롬 생성후 url 끌어오기 성공")
-    a = url.split(".")
-    # -----------------------FACEBOOK---------------------------------------
-    if a[1] == "facebook":
-        print("페이스북 입니다")
-        if "photo/?fbid" in a[2]:
-            # 이미지 창일 경우에 다른 랜딩페이지를 캡쳐하도록 하는 로직 작성
-            print("캡쳐불가 => 랜딩 페이지로 리디렉트")
-            target_substring = "https://www.facebook.com/"
-
-            # 모든 <a> 태그 찾기
-            a_elements = driver.find_elements(By.XPATH,"//h2//span//span//a")
-
-            # <a> 태그 반복 및 href 속성 확인 후 클릭
-            for a_element in a_elements:
-                href = a_element.get_attribute("href")  # href 속성 가져오기
-                print(href)
-                if href and target_substring in href:
-                    a_element.click()
-                    break  # 원하는 엘리먼트를 찾았으면 반복문 종료
-            print(driver.current_url)
-            wait = WebDriverWait(driver, 10)
-            element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "img")))
-            time.sleep(2)
-            # 캡쳐할 랜딩 페이지일 경우에 캡쳐하는 로직 작성
-        wait = WebDriverWait(driver, 10)
-        element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "img")))
-        driver.implicitly_wait(3)
-        # elementExclude(driver, "x1s65kcs")
-        elementExclude(driver,xpath='//*[@role="navigation"]')
-        width = 1600
-        height = 850
-        settingDriverSize(driver,width,height)
-        screenShot(filename)
-    # -----------------------YOUTUBE---------------------------------------
-    elif a[1] == "youtube":
-        print("유튜브 입니다")
-        # todo => 유뷰트 관련 로직 작성/ 클래스값 확인 / 스크롤바 옵션 추가하기
-        wait = WebDriverWait(driver, 10)
-        element = EC.presence_of_all_elements_located((By.CLASS_NAME, "yt-img-shadow"))
-        driver.implicitly_wait(4)
-        width = 1600
-        height = 850
-        settingDriverSize(driver,width,height)
-        screenShot(filename)
-        driver.get(url)
-        wait = WebDriverWait(driver, 10)
-        element = EC.presence_of_all_elements_located((By.CLASS_NAME, "yt-img-shadow"))
-        time.sleep(4)
-        # 스크린샷 캡처
-        screenShot(filename)
-    # -----------------------INSTAGRAM---------------------------------------
-    elif a[1] == "instagram":
-        wait = WebDriverWait(driver, 20)
-        element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "canvas")))
-        time.sleep(4)
-        print("인스타그램 입니다")
-        # todo => 인스타그램 관련 로직 작성/ 클래스값 확인/ 옵션 추가하기
-        # elementExclude(driver, xpath= "//span//div//a")
-        elementExclude(driver=driver,className="_acun")
-        elementExclude(driver= driver,className="_ab8q")
-        try:
-            # X 버튼을 찾습니다.
-            x_button = driver.find_element(By.CLASS_NAME, "_abn5")
-            # X 버튼을 클릭합니다.
-            x_button.click()
-        except:
-            # 엘리먼트를 찾지 못한 경우에 대한 예외 처리
-            print("X 버튼을 찾을 수 없습니다.")
-        # btn.click()
-        # btn.send_keys(Keys.ENTER)
-        element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "canvas")))
-        width = 1300
-        height = 900
-        settingDriverSize(driver,width, height)
-        screenShot(filename)
-    # -----------------------ETC---------------------------------------
-    else:
-        wait = WebDriverWait(driver, 20)
-        element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "img")))
-        time.sleep(1)
-        width = driver.execute_script("return document.body.scrollWidth")
-        height = driver.execute_script("return document.body.scrollHeight")
-        print('스크롤 설정 성공')
-        driver.set_window_size(width, height)
-        print("윈도우 사이즈 설정 성공")
-        screenShot(filename)
-
-    return '캡처 성공'
-
-# ------------ api 함수 ------------
 
 @api_view(['POST'])
 def capture_one(request):
+    driver = webdriver.Chrome(options=chrome_options)
+    # 디버깅을 위한 드라이버 로깅 활성화
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument("--log-level=0")
+
     data = request.data
-    message = capture(data)
+    message = capture(data, driver)
     return Response({"message": message})
+
 @api_view(['POST'])
 def capture_list(request):
+    driver = webdriver.Chrome(options=chrome_options)
+    # 디버깅을 위한 드라이버 로깅 활성화
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument("--log-level=0")
+
     page_list = request.data
     for page in page_list:
-        capture(page)
+        capture(page, driver)
     return Response({"message":"캡쳐 성공"})
 
 
